7df4678a86886c611b1fe37796a8b7db3efb3a20
Wibowoharianja <221113032@students.'mikroskil.ac.id>
2025-11-08 17:13:09 +0700

    update

--- config/mysql.js
+++ config/mysql.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 let db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "root",
+  password: process.env.DB_PASS || "",
   database: process.env.DB_NAME || "todo_db",
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
